83efcea47726c2a37c361d9242a2b506cfe4bb4a
axtck <alexandertack@hotmail.be>
2022-09-28 23:07:07 +0200

    feat: add db admin repository

--- src/core/repository.ts
+++ src/core/repository.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigHelper } from 'src/config/config.helper';
 import { Database } from 'src/database/database';
 
 @Injectable()
 export class Repository {
-  constructor(public readonly database: Database) {}
+  constructor(public readonly database: Database, public readonly configHelper: ConfigHelper) {}
 }
--- src/database/admin/database-admin.ts
+++ src/database/admin/database-admin.ts
@@ -3,14 +3,18 @@ import { ConfigService } from '@nestjs/config';
 import { readdir } from 'fs/promises';
 import * as path from 'path';
 import { ConfigHelper, IDatabaseConfig } from 'src/config/config.helper';
-import { Id, QueryString } from 'src/types/core.types';
+import { DateService } from 'src/core/dates.service';
+import { Null } from 'src/types/core.types';
 import { Database } from '../database';
+import { DatabaseAdminRepository } from './database-admin.repository';
 
 @Injectable()
 export class DatabaseAdmin {
   constructor(
     private readonly database: Database,
     private readonly logger: Logger,
+    private readonly databaseAdminRepository: DatabaseAdminRepository,
+    private readonly dateService: DateService,
     private readonly configHelper: ConfigHelper,
     private readonly configService: ConfigService,
   ) {}
@@ -18,18 +22,11 @@ export class DatabaseAdmin {
   private readonly dbConfig: IDatabaseConfig = this.configHelper.getDatabaseConfig();
 
   public async setSchema(): Promise<void> {
-    const getSchemaQuery: QueryString = `
-      SELECT schema_name 
-      FROM information_schema.schemata 
-      WHERE schema_name = '${this.dbConfig.schema}'
-    `;
-
-    const existingSchema = await this.database.queryOneOrDefault(getSchemaQuery);
-
+    const existingSchema: Null<{ schema_name: string }> = await this.databaseAdminRepository.getSchema();
     if (existingSchema) return;
 
-    await this.database.query(`CREATE SCHEMA ${this.dbConfig.schema} AUTHORIZATION ${this.dbConfig.user}`);
-    await this.database.query(`ALTER DATABASE ${this.dbConfig.database} SET search_path TO ${this.dbConfig.schema}`);
+    await this.databaseAdminRepository.createSchema();
+    await this.databaseAdminRepository.setSearchPath();
   }
 
   public async runMigrations(migrationsFolderPath: string): Promise<void> {
@@ -54,13 +51,10 @@ export class DatabaseAdmin {
 
       // sort based on timestamp (chronological order)
       const sortedMigrationsToRun: string[] = migrationsToRun.sort((a, b) => {
-        return this.convertFilename(a).id - this.convertFilename(b).id;
+        return +this.convertFilename(a).id - +this.convertFilename(b).id;
       });
       this.logger.log(
-        `migrations to run (in order): ${migrationsToRun
-          .map((m) => this.convertFilename(m).id)
-          .join(', ')
-          .slice(0, -2)}.`,
+        `migrations to run (in order): ${migrationsToRun.map((m) => this.convertFilename(m).id).join(', ')}`,
         DatabaseAdmin.name,
       );
 
@@ -89,76 +83,41 @@ export class DatabaseAdmin {
   }
 
   private async createMigrationsTable(): Promise<void> {
-    const getMigrationsTableQuery: QueryString = `
-      SELECT table_name FROM information_schema.tables 
-      WHERE table_schema = '${this.dbConfig.schema}' AND table_name = 'migrations'
-    `;
+    const existingTable: Null<{ table_name: string }> = await this.databaseAdminRepository.getMigrationTable();
 
-    const existingTable: unknown = await this.database.queryOneOrDefault(getMigrationsTableQuery);
     if (existingTable) {
       this.logger.log('migrations table not created (exists)', DatabaseAdmin.name);
       return;
     }
 
-    const createMigrationsTableQuery: QueryString = `
-      CREATE TABLE ${this.dbConfig.schema}.migrations (
-        "id" int8 NOT NULL,
-        "name" varchar(100) NOT NULL,
-        "succeeded" bool NOT NULL,
-        "created" timestamp NOT NULL,
-        "executed" timestamp NOT NULL,
-        CONSTRAINT migrations_pk PRIMARY KEY ("id")
-      )
-    `;
-
-    await this.database.query(createMigrationsTableQuery);
+    await this.databaseAdminRepository.createMigrationTable();
     this.logger.log('successfully created migrations table', DatabaseAdmin.name);
   }
 
   private async insertOrUpdateMigration(migrationFileInfo: IMigrationFileInfo, succeeded: boolean): Promise<void> {
-    const insertMigrationQuery: QueryString = `
-      INSERT INTO ${this.dbConfig.schema}.migrations
-      ("id", "name", "succeeded", "created", "executed") 
-      VALUES ($1, $2, $3, $4, $5)
-      ON CONFLICT ("id") DO UPDATE 
-      SET "succeeded" = excluded.succeeded, "executed" = excluded.executed
-    `;
-
-    const parameters: Array<number | string | Date | boolean> = [
+    const parameters: unknown[] = [
       migrationFileInfo.id, // id
       migrationFileInfo.name, // name
       succeeded, // succeeded
-      new Date(migrationFileInfo.id), // created
-      new Date(), // executed
+      this.dateService.getByUnixTimestamp(+migrationFileInfo.id), // created
+      this.dateService.getNow(), // executed
     ];
 
-    await this.database.query(insertMigrationQuery, parameters);
-  }
-
-  private async getStoredMigrations(): Promise<Array<{ id: string }>> {
-    const getStoredMigrationsQuery: QueryString = `SELECT id FROM ${this.dbConfig.schema}.migrations`;
-    const storedMigrations: Array<{ id: string }> = await this.database.query(getStoredMigrationsQuery);
-    return storedMigrations;
-  }
-
-  private async getFailedMigrations(): Promise<Array<{ id: string }>> {
-    const getMigrationsQuery: QueryString = `SELECT id FROM ${this.dbConfig.schema}.migrations WHERE succeeded = FALSE`;
-    const failedMigrations: Array<{ id: string }> = await this.database.query(getMigrationsQuery);
-    return failedMigrations;
+    await this.databaseAdminRepository.insertMigration(parameters);
   }
 
   private async getMigrationsToRun(migrationFiles: string[]): Promise<string[]> {
-    const storedMigrationIds: Id[] = (await this.getStoredMigrations()).map((m) => Number(m.id));
-    const failedMigrationIds: Id[] = (await this.getFailedMigrations()).map((m) => Number(m.id));
+    const storedMigrationIds: Array<{ id: string }> = await this.databaseAdminRepository.getStoredMigrationIds();
+    const failedMigrationIds: Array<{ id: string }> = await this.databaseAdminRepository.getFailedMigrationIds();
 
     // filter out new migrations
     const newMigrations: string[] = migrationFiles.filter((m) => {
-      return !storedMigrationIds.includes(this.convertFilename(m).id);
+      return !storedMigrationIds.map((m) => m.id).includes(this.convertFilename(m).id);
     });
 
     // filter out failed migrations
     const failedMigrations: string[] = migrationFiles.filter((m) => {
-      return failedMigrationIds.includes(this.convertFilename(m).id);
+      return failedMigrationIds.map((m) => m.id).includes(this.convertFilename(m).id);
     });
 
     const migrationsToRun: string[] = [...newMigrations, ...failedMigrations]; // concat new and failed migrations
@@ -170,12 +129,16 @@ export class DatabaseAdmin {
       .slice(0, -3)
       .split(/_(.+)/)
       .filter((x) => x); // slice of extention and split on first _
-    if (!parts || parts.length !== 2) throw new Error(`converting filename '${filename}' failed`);
-    if (isNaN(Number(parts[0]))) throw new Error('migration timestamp missing in filename');
+
+    if (!parts || parts.length !== 2) {
+      throw new Error(`converting filename '${filename}' failed`);
+    }
+
     const info: IMigrationFileInfo = {
-      id: Number(parts[0]),
+      id: parts[0],
       name: parts[1],
     };
+
     return info;
   }
 }
@@ -185,6 +148,6 @@ interface IMigrationFile {
 }
 
 interface IMigrationFileInfo {
-  id: Id;
+  id: string;
   name: string;
 }
